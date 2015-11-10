7b380dd6bbdadccc28f451a2a1b3815b17e13bfd
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2015-11-10 21:05:39 -0200

    Small changes

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -12,17 +12,8 @@ function hideBackTop(){
 	document.getElementById('backToTopButton').style.visibility = 'hidden';
 }
 
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-    if (target.length) {
-    $('html,body').animate({
-      scrollTop: target.offset().top
-    }, 1000);
-    return false;
-    }
-  }
-  });
-});
+function hideIfTop(){
+	if (window.pageYOffset==0){
+	        document.getElementById("backToTopButton").style.visibility = "hidden";
+	}
+}
