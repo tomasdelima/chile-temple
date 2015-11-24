2810bd37421a42fdbb1173eaca950ae78b17d5fb
Tomas Duringer Jacques de Lima <tomas.lima@dtmtec.com.br>
2015-11-24 20:07:51 -0200

    Creates admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017184940) do
+ActiveRecord::Schema.define(version: 20151124220626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "groups", force: :cascade do |t|
     t.datetime "created_at", null: false
